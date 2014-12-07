4dd01212cbddfa764dda87ea5e6efb641a366d74
jhfjhfj1 <jhfjhfj1@gmail.com>
2014-12-07 16:29:25 +0800

    unknown

--- graph theory/SPFA(vector).cpp
+++ graph theory/SPFA(vector).cpp
@@ -1,76 +1,53 @@
 #define MAX_NODE_NUM 105
 #define INF 0x3f3f3f3f
 
-struct Edge
-{
-    int v;
-    int length;
-    Edge()
-    {}
-    Edge(int v, int length):v(v), length(length)
-    {}
-};
-
-int dist[MAX_NODE_NUM];
-int node_num, road_num;
-bool vis[MAX_NODE_NUM];
+int node_num, edge_num;
+long long dist[MAX_NODE_NUM];
+vector<pair<int, int> > edge[MAX_NODE_NUM];
 int push_cnt[MAX_NODE_NUM];
-vector<vector<Edge> > G;
 bool in_queue[MAX_NODE_NUM];
-
-void addedge(vector<vector<Edge> > &G, int u, int v, int length)
-{
-    G[u].push_back(Edge(v, length));
-}
-
-void iterate()
-{
-    for (int i = 0; i < node_num; i++)
-    {
-        for (vector<Edge>::iterator j = G[i].begin(); j != G[i].end(); j++)
- 	{}
-    }
-}
 
 bool relax(int &dist, int length)
 {
-    if (dist > length)
-    {
-        dist = length;
-        return true;
-    }
-    return false;
+	if (dist > length)
+	{
+		dist = length;
+		return true;
+	}
+	return false;
 }
 
 int SPFA(int origin, int destination)
 {
-    fill(dist, dist + node_num, INF);
-    memset(push_cnt, 0, sizeof(push_cnt));
-    memset(in_queue, 0, sizeof(in_queue));
-    queue<int> q;
-    q.push(origin);
-    in_queue[origin] = true;
-    dist[origin] = 0;
-    bool unbound = false;
-    while (!q.empty())
-    {
-        int temp = q.front();
-        q.pop();
-	in_queue[temp] = false;
-        for (vector<Edge>::iterator i = G[temp].begin(); i != G[temp].end(); i++)
-        {
-            if (relax(dist[i->v], dist[temp] + i->length) && !in_queue[i->v]) 
-            {
-                q.push(i->v);
-		in_queue[i->v] = true;
-                push_cnt[i->v]++;
-                if (push_cnt[i->v] > node_num)
-                {
-                    return -1;		//negative ring;
-                }
-            }
-        }
-    }
-    return dist[destination];
+	fill(dist, dist + node_num, INF);
+	fill(push_cnt, push_cnt + node_num, 0);
+	fill(in_queue, in_queue + node_num, 0);
+	queue<int> q;
+	q.push(origin);
+	in_queue[origin] = true;
+	dist[origin] = 0;
+	bool unbound = false;
+	while (!q.empty())
+	{
+		int u = q.front();
+		q.pop();
+		in_queue[u] = false;
+		for (int i = 0; i < edge[u].size(); i++)
+		{
+			int v = edge[u][i].first;
+			int w = edge[u][i].second;
+			if (relax(dist[v], dist[u] + w) && !in_queue[v]) 
+			{
+				q.push(i->v);
+				in_queue[i->v] = true;
+				push_cnt[i->v]++;
+				if (push_cnt[i->v] > node_num)
+				{
+					return -1;		//negative ring;
+				}
+			}
+		}
+	}
+	return dist[destination];
 }
 
--- graph theory/dijkstra_Edge_pq.cpp
+++ graph theory/dijkstra_Edge_pq.cpp
@@ -25,42 +25,28 @@ void add_edge(int u, int v, int w)
 }
 
 
-bool vis[MAX_NODE_NUM];
 long long dist[MAX_NODE_NUM];
 priority_queue<Edge> pq;
 
 void dijkstra(int source)
 {
-	memset(vis, 0, sizeof(vis));
 	fill(dist, dist + node_num, INF);
 	dist[source] = 0;
 	pq.push(Edge(0, -1, 0));
 	while (!pq.empty())
 	{
-		int min_node = -1;
 		Edge a;
-		while (!pq.empty())
-		{
-			a = pq.top();
-			min_node = a.v;
-			if (!vis[a.v])
-			{
-				break;
-			}
-			pq.pop();
-		}
-		if (min_node == -1)
-		{
-			break;
-		}
-		vis[min_node] = true;
-		for (int i = head[min_node]; i!= -1; i = edge[i].next)
+		a = pq.top();
+		pq.pop();
+		if (dist[a.v] != a.w)
+			continue;
+		for (int i = head[a.v]; i!= -1; i = edge[i].next)
 		{
 			int v = edge[i].v;
-			long long w = edge[i].w;
-			if (dist[v] > dist[min_node] + w)
+			long long w = edge[i].w + a.w;
+			if (dist[v] > w)
 			{
-				dist[v] = dist[min_node] + w;
+				dist[v] = w;
 				pq.push(Edge(v, -1, dist[v]));
 			}
 		}
