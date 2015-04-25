b0ba3d79daadd6f33dddfe664c74c09dce7015a1
jhfjhfj1 <jhfjhfj1@gmail.com>
2015-04-26 01:44:10 +0800

    prime_sieve

--- number theory/prime_sieve.cpp
+++ number theory/prime_sieve.cpp
@@ -4,22 +4,22 @@ int prime_num;
 
 void getprime(int n)
 {
-	int i, j;
-	int s, e = (int)(sqrt(0.0 + n) + 1);
 	prime_num = 0;
-	memset(is_prime, 1, sizeof(is_prime));
+	fill_n(is_prime, n + 1, true);
 	prime[prime_num++] = 2;
-	is_prime[0] = is_prime[1] = 0;
-	for (i = 4; i < n; i += 2) 
-		is_prime[i] = 0;
-	for (i = 3; i < e; i += 2)
-		if (is_prime[i])
-		{
-			prime[prime_num++] = i;
-			for (s = i * 2, j = i * i; j < n; j += s)
-				is_prime[j] = 0;
-		}
-	for ( ; i < n; i += 2)
-		if (is_prime[i])
-			prime[prime_num++] = i;
+	is_prime[0] = is_prime[1] = false;
+
+	for (int i = 4; i <= n; i += 2) 
+		is_prime[i] = false;
+
+	for (int i = 3; i <= n; i += 2)
+	{
+		if (!is_prime[i])
+			continue;
+		prime[prime_num++] = i;
+		if (n / i < i)
+			continue;
+		for (int j = i * i; j <= n; j += i * 2)
+			is_prime[j] = false;
+	|
 }
